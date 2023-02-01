09466423dca8585d5ee40024c6fe1a5c59451206
Leo <leoliu5550@gmail.com>
2023-02-01 15:43:48 +0800

    fixed image size in model problem

--- TEST/test_model.py
+++ TEST/test_model.py
@@ -3,22 +3,32 @@ import torch
 import sys
 sys.path.append("/home/lcliu/Documents/DDN")
 from model import *
+'''
+should preprocess the image size to 2^n
+'''
+
+
 
 class TestCNNBlock:
     def test_CNN(self):
-        x = torch.ones(1,3,200,200) # 3 times = 56x56
-        model = CNNBlock(in_channel=3,out_channel=64,kernel_size = 3,stride = 2)
-        assert model(x).shape == torch.Size([1, 64, 99, 99])
+        x = torch.ones(1,3,256,256) # 3 times = 56x56
+        model = CNNBlock(
+            in_channel=3,
+            out_channel=64,
+            kernel_size = 3,
+            padding = 1,
+            stride = 2)
+        assert model(x).shape == torch.Size([1, 64, 128, 128])
 
 class TestResNet:
     def test_BTNK1(self):
-        x = torch.ones(1,64,112,112)
+        x = torch.ones(1, 64, 128, 128)
         model1 = BTNK1(in_channel=64,stride = 1)
-        assert model1(x).shape == torch.Size([1, 256, 112, 112])
+        assert model1(x).shape == torch.Size([1, 256, 128, 128]) # --> 64 * 4 as half = False
 
-        x2 = torch.ones(1,256,56,56) 
+        x2 = torch.ones(1,256,128,128) 
         model2 = BTNK1(in_channel=256,half = True,stride = 1)
-        assert model2(x2).shape == torch.Size([1, 512, 56, 56])
+        assert model2(x2).shape == torch.Size([1, 512, 128, 128]) # --> 64 * 2 as half = True
 
     def test_BTNK2(self):
         x = torch.ones(1,10,200,200) 
@@ -26,34 +36,36 @@ class TestResNet:
         assert model(x).shape == torch.Size([1,10,200,200])
 
     def test_R1Block(self):
-        x = torch.ones(1,3,224,224) 
+        x = torch.ones(1,3,256,256) 
         model = R1Block(in_channel=3)
-        assert model(x).shape == torch.Size([1, 64, 112, 112])
+        assert model(x).shape == torch.Size([1, 64, 128, 128])
 
     def test_R2Block(self):
-        x = torch.ones(1, 64, 112, 112) 
+        x = torch.ones(1, 64, 128, 128) 
         model = R2Block(in_channel=64)
-        assert model(x).shape == torch.Size([1, 256, 56, 56])
+        assert model(x).shape == torch.Size([1, 256, 64, 64])
         
     def test_R3Block(self):
-        x = torch.ones(1, 256, 56, 56) 
+        x = torch.ones(1, 256, 64, 64) 
         model = R3Block(in_channel=256)
-        assert model(x).shape == torch.Size([1, 512, 28, 28])
+        assert model(x).shape == torch.Size([1, 512, 32, 32])
 
     def test_R4Block(self):
-        x = torch.ones(1, 512, 28, 28) # 3 times = 56x56
+        x = torch.ones(1, 512, 32, 32) # 3 times = 56x56
         model = R4Block(in_channel=512)
-        assert model(x).shape == torch.Size([1, 1024, 14, 14])
+        assert model(x).shape == torch.Size([1, 1024, 16, 16])
 
     def test_R5Block(self):
-        x = torch.ones(1, 1024, 14, 14) # 3 times = 56x56
+        x = torch.ones(1, 1024, 16, 16) # 3 times = 56x56
         model = R5Block(in_channel=1024)
-        assert model(x).shape == torch.Size([1, 2048, 7, 7])
+        assert model(x).shape == torch.Size([1, 2048, 8, 8])
 
 
 class TestMFN:
     def test_MFN(self):
         model = MFN()
-        x = torch.rand(1,3,320,320)
-        assert model(x).shape == torch.Size([1,3840,20,20])
+        x = torch.rand(1,3,256,256)
+        print(model(x).shape)
+        assert model(x).shape == torch.Size([1,3840,16,16])
 
+## Image Size from 256*256 to 16*16
--- model.py
+++ model.py
@@ -7,7 +7,8 @@ class CNNBlock(nn.Module):
         super().__init__()
         self.act = act
         self.conv = nn.Conv2d(in_channels = in_channel,
-                        out_channels = out_channel,**kwargs)
+                        out_channels = out_channel
+                        ,**kwargs)
         self.bn = nn.BatchNorm2d(out_channel)
         self.ReLU = nn.ReLU()
 
@@ -26,6 +27,7 @@ class BTNK1(nn.Module):
             out_channel = in_channel//2
         else:
             out_channel = in_channel
+
         self.path1 = nn.Sequential(
             CNNBlock(in_channel=in_channel,out_channel=out_channel,kernel_size = 1,**kwargs),
             CNNBlock(in_channel=out_channel,out_channel=out_channel,kernel_size = 3,stride =1,padding =1),
@@ -36,8 +38,8 @@ class BTNK1(nn.Module):
         self.ReLU = nn.ReLU()
     def forward(self,x):
         x0 = x
-        x1 = x
         x0 = self.path1(x0)
+        x1 = x
         x1 = self.CNN(x1)
         x = x0 + x1
         x = self.ReLU(x)
@@ -55,8 +57,8 @@ class BTNK2(nn.Module):
         self.ReLU = nn.ReLU()
     def forward(self,x):
         x0 = x
-        x1 = x
         x0 = self.path1(x0)
+        x1 = x
         x1 = self.CNN(x1)
         x = x0 + x1
         x = self.ReLU(x)
@@ -143,14 +145,17 @@ class R5Block(nn.Module):
 class MFN(nn.Module):
     def __init__(self):
         super().__init__()
-        in_list = [3,64,256,512,1024,2048]
+        in_list = [3,64,256,512,1024,2048] #in_channel in R1 R2 R3 R4 R5 
+
         self.r1 = R1Block(in_list[0])
         self.r2 = R2Block(in_list[1])
+
         self.r2Conv = nn.Sequential(
             nn.Conv2d(in_channels = in_list[2],out_channels = in_list[2],kernel_size=3,stride=2,padding =1),
             nn.Conv2d(in_channels = in_list[2],out_channels = in_list[2],kernel_size=3,stride=2,padding =1),
             nn.Conv2d(in_channels = in_list[2],out_channels = in_list[2],kernel_size=1,stride=1)
         )
+
         self.r3 = R3Block(in_list[2])
         self.r3Pool = nn.MaxPool2d(kernel_size = 2,stride=2)
         self.r4 = R4Block(in_list[3])
@@ -160,6 +165,7 @@ class MFN(nn.Module):
             nn.ConvTranspose2d(in_channels = in_list[5],out_channels = in_list[5],kernel_size = 4,stride=2,padding =1),
             nn.Conv2d(in_channels = in_list[5],out_channels = in_list[5],kernel_size=1,stride=1)
         )
+
     def forward(self,x):
         R_output= []
         x = self.r1(x)
@@ -177,7 +183,9 @@ class MFN(nn.Module):
         R_output.append(x) # R_output+= x
         R_output[-1] = self.r5Conv(R_output[-1])
         for i,out in enumerate(R_output):
+            print(out.shape)
             R_output[i] = f.normalize(out,p=2) #,dim=-1
+
         output = torch.cat(R_output,dim=1)
         print(output.shape)
         return output
